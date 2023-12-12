5cf4a1b0a744a801cd1cd855a3bafa7045da8ac1
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2023-12-13 01:26:08 +0200

    sign-up and sign-in pages

---
+++ js/home.js
@@ -0,0 +1,50 @@
+let imagesContainer = document.getElementById("images"),
+    trails = document.getElementById("trails"),
+    leftArrow = document.getElementById("left-arrow"),
+    rightArrow = document.getElementById("right-arrow"),
+    activeTrail = trails.querySelector(".active");
+
+document.body.onresize = function () {
+    activateTrail(activeTrail);
+};
+
+trails.querySelectorAll("span.trail").forEach(function (el) {
+    el.addEventListener("click", () => {
+        activeTrail = el;
+        activateTrail(el);
+    });
+});
+
+rightArrow.addEventListener("click", () => {
+    if (activeTrail.nextElementSibling == null) {
+        activeTrail = trails.querySelector("span:first-child");
+    } else {
+        activeTrail = activeTrail.nextElementSibling;
+    }
+    activateTrail(activeTrail);
+});
+
+leftArrow.addEventListener("click", () => {
+    if (activeTrail.previousElementSibling == null) {
+        activeTrail = trails.querySelector("span:last-child");
+    } else {
+        activeTrail = activeTrail.previousElementSibling;
+    }
+    activateTrail(activeTrail);
+});
+
+function activateTrail(trail) {
+    trails.querySelectorAll("span.trail").forEach(function (el) {
+        el.classList.remove("active");
+    });
+    trail.classList.add("active");
+    imagesContainer.style.left =
+        -(activeTrail.dataset.order - 1) *
+            document.getElementById("banner").getBoundingClientRect().width +
+        "px";
+        console.log(document.getElementById("banner").getBoundingClientRect().width);
+}
+
+// setInterval(() => {
+//     rightArrow.click();
+// }, 5000);
