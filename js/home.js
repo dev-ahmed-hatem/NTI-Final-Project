4492fe1b57283a180f2a8df5c26323781cea680d
dev-ahmed-hatem <ahmedhatemezzathelal@gmail.com>
2023-12-15 04:55:30 +0200

    finished home page with responsive layout

--- js/home.js
+++ js/home.js
@@ -38,13 +38,46 @@ function activateTrail(trail) {
         el.classList.remove("active");
     });
     trail.classList.add("active");
-    imagesContainer.style.left =
-        -(activeTrail.dataset.order - 1) *
-            document.getElementById("banner").getBoundingClientRect().width +
-        "px";
-        console.log(document.getElementById("banner").getBoundingClientRect().width);
+    imagesContainer.style.transform = `translateX(${
+        -(activeTrail.dataset.order - 1) * 100
+    }%)`;
 }
 
 // setInterval(() => {
 //     rightArrow.click();
 // }, 5000);
+
+// fly menu
+
+let flyMenu = document.getElementById("fly-menu"),
+    menuBtn = document.getElementById("menu-btn"),
+    menuClsBtn = document.getElementById("menu-cls-btn"),
+    cartMenu = document.getElementById("cart-menu"),
+    cartBtn = document.getElementById("cart-btn"),
+    cartClsBtn = document.getElementById("cart-cls-btn"),
+    hider = document.getElementById("hider");
+
+menuBtn.addEventListener("click", () => {
+    flyMenu.classList.add("active");
+    hider.classList.add("active");
+    // document.documentElement.classList.add("stop-scrolling");
+});
+
+menuClsBtn.addEventListener("click", () => {
+    flyMenu.classList.remove("active");
+    hider.classList.remove("active");
+    // document.documentElement.classList.remove("stop-scrolling");
+});
+
+
+cartBtn.addEventListener("click", () => {
+    cartMenu.classList.add("active");
+    hider.classList.add("active");
+    // document.documentElement.classList.add("stop-scrolling");
+});
+
+cartClsBtn.addEventListener("click", () => {
+    cartMenu.classList.remove("active");
+    hider.classList.remove("active");
+    // document.documentElement.classList.remove("stop-scrolling");
+});
